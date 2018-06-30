bf3797beeb90d258378298d817749c1ed9f3e274
Joey Palmieri <joeypalmieri@hotmail.com>
2018-06-30 02:22:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -13,5 +13,5 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return [element, ...array]
+  return [...element, array]
 }
